41c00b7b33d5acc0bb6f082c29880c43ef4ef67c
Ivy Singh <ivysingh99@gmail.com>
2025-09-07 11:28:54 +0530

    initialise db

--- models/listing.js
+++ models/listing.js
@@ -5,13 +5,16 @@ const listingSchema = new Schema({
   title: { type: String, required: true },
   description: String,
   image: {
-    type: String,
-    default:
-      "https://plus.unsplash.com/premium_photo-1666863909125-3a01f038e71f?q=80&w=1572&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    set: (v) =>
-      v === ""
-        ? "https://plus.unsplash.com/premium_photo-1666863909125-3a01f038e71f?q=80&w=1572&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        : v,
+    filename: String,
+    url: {
+      type: String,
+      default:
+        "https://plus.unsplash.com/premium_photo-1666863909125-3a01f038e71f?q=80&w=1572&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      set: (v) =>
+        v === ""
+          ? "https://plus.unsplash.com/premium_photo-1666863909125-3a01f038e71f?q=80&w=1572&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          : v,
+    },
   },
   price: Number,
   location: String,
