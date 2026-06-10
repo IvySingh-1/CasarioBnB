ad135528361a6385fc0932c23e1ad544ef90a32b
Ivy Singh <ivysingh99@gmail.com>
2026-06-10 10:33:44 +0530

    flash success includes

--- routes/review.js
+++ routes/review.js
@@ -29,7 +29,7 @@ router.post(
 
     await newReview.save();
     await listing.save();
-
+req.flash("success", "New Review Created!");
     res.redirect(`/listings/${listing._id}`);
   }),
 );
@@ -45,7 +45,7 @@ router.delete(
         });
 
         await Review.findByIdAndDelete(reviewId);
-
+req.flash("success", "Review Deleted!");
         res.redirect(`/listings/${id}`);
     })
 );
