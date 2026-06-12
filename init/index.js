3728bf163b436cf26bf47c0948b7fef4aeac84fd
Ivy Singh <ivysingh99@gmail.com>
2026-06-12 13:00:07 +0530

    listing owner

--- init/index.js
+++ init/index.js
@@ -18,6 +18,10 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "6a2baea354d595e4991d3fab",
+  }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
